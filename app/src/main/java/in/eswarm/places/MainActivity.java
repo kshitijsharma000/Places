8d87c01b5f712edcab67a492460e52e2ae9ef5b0
Eswar <eswar.malla@gmail.com>
2016-02-07 15:03:25 +0530

    mainview

--- app/src/main/java/in/eswarm/places/MainActivity.java
+++ app/src/main/java/in/eswarm/places/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.app.SearchManager;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,9 +23,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.json.JSONObject;
@@ -30,19 +35,13 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, DataRetriever.DataListener, AppBarLayout.OnOffsetChangedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, DataRetriever.DataListener {
+
 
     RecyclerView mRecyclerView;
     ProgressDialog mDialog;
     AdapterCategoryList mAdapter;
     DataRetriever mDataRetriever;
-    FrameLayout mImageFrame;
-    TextView mCityNameMin;
-    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.4f;
-    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.6f;
-    private static final int ALPHA_ANIMATIONS_DURATION              = 200;
-    private boolean mIsTheTitleVisible = false;
-    private boolean mIsTheTitleContainerVisible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +49,16 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //setStatusBarColor(Color.WHITE);
 
-        if(getSupportActionBar() != null)
+        CollapsingToolbarLayout collapsingToolbarLayout =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing);
+
+        collapsingToolbarLayout.setTitle("Bengaluru");
+
+        /*if(getSupportActionBar() != null)
             getSupportActionBar().setDisplayShowTitleEnabled(false);
-
+        */
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,8 +76,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ImageView imageView = (ImageView) findViewById(R.id.cityImage);
-        imageView.setImageResource(R.drawable.blore);
+        //ImageView imageView = (ImageView) findViewById(R.id.cityImage);
+        //imageView.setImageResource(R.drawable.blore);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerCategoryList);
 
@@ -94,8 +99,8 @@ public class MainActivity extends AppCompatActivity
             }
         }));
 
-        mCityNameMin = (TextView) findViewById(R.id.city_name_min);
-        mImageFrame = (FrameLayout) findViewById(R.id.imageFrame);
+        //mCityNameMin = (TextView) findViewById(R.id.city_name_min);
+        //mImageFrame = (LinearLayout) findViewById(R.id.imageFrame);
         mDialog = new ProgressDialog(this);
         mDialog.setMessage("Loading...");
         mDialog.setTitle("Fetching Data");
@@ -105,6 +110,16 @@ public class MainActivity extends AppCompatActivity
         mDataRetriever = new DataRetriever(this);
         mDataRetriever.getCategories("Bengaluru");
         setRecyclerView();
+
+
+    }
+
+    private void setStatusBarColor(int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(color);
+        }
     }
 
     private void setRecyclerView() {
@@ -203,58 +218,6 @@ public class MainActivity extends AppCompatActivity
         mDialog.hide();
     }
 
-    @Override
-    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-        int maxScroll = appBarLayout.getTotalScrollRange();
-        float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
-
-        handleAlphaOnTitle(percentage);
-        handleToolbarTitleVisibility(percentage);
-    }
-
-    private void handleToolbarTitleVisibility(float percentage) {
-        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
-
-            if(!mIsTheTitleVisible) {
-                startAlphaAnimation(mCityNameMin, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
-                //startAlphaAnimation(mCityNameMin, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
-                mIsTheTitleVisible = true;
-            }
-
-        } else {
-            if (mIsTheTitleVisible) {
-                startAlphaAnimation(mCityNameMin, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
-                //startAlphaAnimation(mTitleTime, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
-                mIsTheTitleVisible = false;
-            }
-        }
-    }
-
-    private void handleAlphaOnTitle(float percentage) {
-        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
-            if(mIsTheTitleContainerVisible) {
-                startAlphaAnimation(mImageFrame, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
-                mIsTheTitleContainerVisible = false;
-            }
-
-        } else {
-
-            if (!mIsTheTitleContainerVisible) {
-                startAlphaAnimation(mImageFrame, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
-                mIsTheTitleContainerVisible = true;
-            }
-        }
-    }
-
-    public static void startAlphaAnimation (View v, long duration, int visibility) {
-        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
-                ? new AlphaAnimation(0f, 1f)
-                : new AlphaAnimation(1f, 0f);
-
-        alphaAnimation.setDuration(duration);
-        alphaAnimation.setFillAfter(true);
-        v.startAnimation(alphaAnimation);
-    }
 
 
 }
